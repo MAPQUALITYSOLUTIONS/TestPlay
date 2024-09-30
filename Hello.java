7024e5a07d8e2e624e2c4e55e0d02a56fc124eb4
MAPQUALITYSOLUTIONS <mapqualitysolutions@gmail.com>
2024-09-30 19:36:04 +0530

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,12 @@
+package basicTest;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Test");
+		System.out.println("Test updated");
+
+	}
+
+}
